854585efe9468a2db12cd74885a9079c37498b5c
tolgazorlu <tolgazorlu17@gmail.com>
2023-05-12 14:08:35 +0300

    blog post done

--- index.js
+++ index.js
@@ -1,35 +1,33 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const ejs = require('ejs');
 const path = require('path');
+const Photo = require('./models/Photo');
 
 const app = express();
+
+//Connect DB
+mongoose.connect('mongodb://localhost/pcat-test-db', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 //TEMPLATE ENGINE
 app.set('view engine', 'ejs');
 
 // MIDDLEWARES
 app.use(express.static('public'));
-
-app.use((req, res, next) => {
-  console.log('Middleware worked..');
-  next();
-});
-
-// app.get('/', (req, res)=>{
-
-//     const photo = {
-//         id: 1,
-//         name: "Photo name",
-//         description: "This is first photo..."
-//     }
-
-//     res.send(photo);
-// })
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //ROUTES
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   //res.sendFile(path.resolve(__dirname, 'temp/index.html'));
-  res.render('index');
+  const photos = await Photo.find({});
+  res.render('index', {
+    photos: photos
+  });
 });
 
 app.get('/about', (req, res) => {
@@ -43,4 +41,13 @@ app.get('/add', (req, res) => {
 const port = 3000;
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda çalışıyor...`);
+});
+
+//Insert data to database
+app.post('/photos', (req, res) => {
+  const addPhoto = async (newTitle, newDesc) => {
+    await Photo.create({ title: newTitle, description: newDesc });
+  };
+  addPhoto(req.body.title, req.body.description);
+  res.redirect('/');
 });
